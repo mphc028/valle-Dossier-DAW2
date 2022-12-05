04af91431c85870ec92e82e40c0a3e7c10d4c87f
mphc028 <mphc028@gmail.com>
2022-12-05 15:12:50 +0100

    Changed Readme

---
+++ public_html/ValleDevelAtuhFbLogin.md
@@ -0,0 +1,10 @@
+# VallePropostaAlumne
+
+## Descripción general
+
+El formulario de registro requerirá un email y una password. Implementará el sistema de la API de Firebase para gestionar los usuarios por "Email and Password". Este sistema se encargará de la lógica, yo desarrollaré los formularios y las funciones que integren la API para guardar las variables introducidas en los formularios.
+
+## Habrá dos versiones de la página:
+
+La versión sin inciar sesión mostrará mi información menos personal, pero no mis proyectos ni mi información de contacto.
+La versión inciando sesión mostrará toda la página y habrá un botón arriba para cerrar sesión.
--- public_html/assets/js/main.js
+++ public_html/assets/js/main.js
@@ -253,22 +253,22 @@
     })
   });
 
-  var cld = cloudinary.Cloudinary.new({ cloud_name: 'demo' });
-  var mainScreen = cld.videoPlayer('main-screen');
-  mainScreen.source("snow_deer");
-  var preview = cld.videoPlayers('.preview', {videojs: { bigPlayButton: false } });
-  var sources = ["snow_deer","sea_turtle","elephants" ];
-  for(var i = 0; i < preview.length; i++){
-      preview[i].on("mouseover",function(event) {event.Player.play()});
-      preview[i].on("mouseout",function(event) {event.Player.videojs.load()});
-      preview[i].on("click",function(event) {playMain(event.Player.currentPublicId())});
-      preview[i].source(sources[i], {transformation: {effect: "preview"}});   
-  }
+  // var cld = cloudinary.Cloudinary.new({ cloud_name: 'demo' });
+  // var mainScreen = cld.videoPlayer('main-screen');
+  // mainScreen.source("snow_deer");
+  // var preview = cld.videoPlayers('.preview', {videojs: { bigPlayButton: false } });
+  // var sources = ["snow_deer","sea_turtle","elephants" ];
+  // for(var i = 0; i < preview.length; i++){
+  //     preview[i].on("mouseover",function(event) {event.Player.play()});
+  //     preview[i].on("mouseout",function(event) {event.Player.videojs.load()});
+  //     preview[i].on("click",function(event) {playMain(event.Player.currentPublicId())});
+  //     preview[i].source(sources[i], {transformation: {effect: "preview"}});   
+  // }
 
-  function playMain(src) {
-      mainScreen.source(src);
-      mainScreen.controls(true);
-      mainScreen.play();
-  }
+  // function playMain(src) {
+  //     mainScreen.source(src);
+  //     mainScreen.controls(true);
+  //     mainScreen.play();
+  // }
   
 })()
